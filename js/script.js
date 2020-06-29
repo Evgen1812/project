0af894238c1d32acb4b99aa79db382399cd6cff7
Evgen <kryuk.evgen@mail.ru>
2020-06-29 10:08:32 +0300

    creat personalMovieDB,movies

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 
-let test = prompt("How are yuo?", '');
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 
-console.log(test);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов", '');
+const b = prompt("На сколько оцените его", '');
+const c = prompt("Один из последних просмотренных фильмов", '');
+const d = prompt("На сколько оцените его", '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
